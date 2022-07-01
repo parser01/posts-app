f4d0a81460f9ad78c3326fe22e1b7284a7e10647
strannik <spacenett55@gmail.com>
2022-07-01 09:53:10 +0300

    changing variable in component doesn't lead to component rerender

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let likes = 0;
+
+	function increment() {
+		likes += 1;
+		console.log(likes);
+	}
+
+	function decrement() {
+		likes -= 1;
+		console.log(likes);
+	}
+
+	return (
+		<div className="App">
+			<h1>{likes}</h1>
+			<button onClick={increment}>Increment</button>
+			<button onClick={decrement}>Decrement</button>
+		</div>
+	);
 }
 
 export default App;
