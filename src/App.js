c27d858ed4a3c239c5e7e37bb38f83a70c5dd819
strannik <spacenett55@gmail.com>
2022-07-01 10:55:47 +0300

    two way binding and controlled input

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import { useState } from "react";
 
 function App() {
 	const [likes, setLikes] = useState(0);
+	const [value, setValue] = useState("TEXT IN INPUT");
+
+	function onChange(event) {
+		console.log(event.target.value);
+		setValue(event.target.value);
+		console.log(event.target.value);
+	}
 
 	return (
 		<div className="App">
 			<h1>{likes}</h1>
+			<h2>{value}</h2>
+			<input type="text" value={value} onChange={onChange} />
 			<button onClick={() => setLikes(likes + 1)}>Increment</button>
 			<button onClick={() => setLikes(likes - 1)}>Decrement</button>
 		</div>
