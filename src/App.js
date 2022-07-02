69a32a08449134adc1bd1db075c052f6dfdea1cf
strannik <spacenett55@gmail.com>
2022-07-02 15:19:13 +0300

    css styles, classes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import ClassCounter from "./components/ClassCounter";
+import "./styles/App.css";
 
 function App() {
 	return (
 		<div className="App">
-			<ClassCounter />
-			<ClassCounter />
-			<ClassCounter />
+			<div className="post">
+				<div className="post__content">
+					<strong>1. JavaScript</strong>
+					<div>JavaScript is a programming language</div>
+				</div>
+				<div className="post__btns">
+					<button>Delete</button>
+				</div>
+			</div>
+			<div className="post">
+				<div className="post__content">
+					<strong>1. JavaScript</strong>
+					<div>JavaScript is a programming language</div>
+				</div>
+				<div className="post__btns">
+					<button>Delete</button>
+				</div>
+			</div>
+			<div className="post">
+				<div className="post__content">
+					<strong>1. JavaScript</strong>
+					<div>JavaScript is a programming language</div>
+				</div>
+				<div className="post__btns">
+					<button>Delete</button>
+				</div>
+			</div>
 		</div>
 	);
 }
