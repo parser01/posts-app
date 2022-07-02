43dcd6437e4d09f7f72ebb8b80e04584f82fc279
strannik <spacenett55@gmail.com>
2022-07-02 15:45:19 +0300

    props

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
+import PostItem from "./components/PostItem";
 import "./styles/App.css";
 
 function App() {
 	return (
 		<div className="App">
-			<div className="post">
-				<div className="post__content">
-					<strong>1. JavaScript</strong>
-					<div>JavaScript is a programming language</div>
-				</div>
-				<div className="post__btns">
-					<button>Delete</button>
-				</div>
-			</div>
-			<div className="post">
-				<div className="post__content">
-					<strong>1. JavaScript</strong>
-					<div>JavaScript is a programming language</div>
-				</div>
-				<div className="post__btns">
-					<button>Delete</button>
-				</div>
-			</div>
-			<div className="post">
-				<div className="post__content">
-					<strong>1. JavaScript</strong>
-					<div>JavaScript is a programming language</div>
-				</div>
-				<div className="post__btns">
-					<button>Delete</button>
-				</div>
-			</div>
+			<PostItem post={{ id: 1, title: "JavaScript", body: "Description" }} />
 		</div>
 	);
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,19 @@
+const PostItem = (props) => {
+	console.log(props);
+
+	return (
+		<div className="post">
+			<div className="post__content">
+				<strong>
+					{props.post.id}. {props.post.title}
+				</strong>
+				<div>{props.post.body}</div>
+			</div>
+			<div className="post__btns">
+				<button>Delete</button>
+			</div>
+		</div>
+	);
+};
+
+export default PostItem;
